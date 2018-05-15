ecb3c1803adce67a87db6135651cb278601b20c7
amgranad <ambergkim@gmail.com>
2018-05-15 11:01:33 -0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/app.jsx';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 let root = document.createElement('div');
 document.body.appendChild(root);
 
-ReactDom.render(<App />, root);
+ReactDom.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, root);
